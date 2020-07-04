b15f69f141cae213c35b4dc289b3e31bcb57ef86
Ricardo Simões <ricardosimoess2001@gmail.com>
2020-07-04 15:41:23 +0000

    added miniscule part of game logic

--- BlackJackServerPrompt/src/org/academiadecodigo/gitbusters/TeamCharlie/BlackJack/CardDeck.java
+++ BlackJackServerPrompt/src/org/academiadecodigo/gitbusters/TeamCharlie/BlackJack/CardDeck.java
@@ -8,10 +8,13 @@ public class CardDeck {
     public static void main(String[] args) {
         CardDeck cardDeck = new CardDeck();
        cardDeck.shuffle();
+
         for(Card card: cardDeck.getDeck()){
             System.out.println(card);
             //System.out.println("value: " + card.getValue());
         }
+        System.out.println(cardDeck.dealCard());
+
     }
 
 
--- BlackJackServerPrompt/src/org/academiadecodigo/gitbusters/TeamCharlie/BlackJack/Game.java
+++ BlackJackServerPrompt/src/org/academiadecodigo/gitbusters/TeamCharlie/BlackJack/Game.java
@@ -1,4 +1,15 @@
 package org.academiadecodigo.gitbusters.TeamCharlie.BlackJack;
 
+import java.util.ArrayList;
+
 public class Game {
+    private Dealer dealer;
+    private ArrayList<Player> players;
+    public Game(Dealer dealer){
+        this.dealer = dealer;
+    }
+
+    public void start(){
+        
+    }
 }
