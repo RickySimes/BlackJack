ca5edf9cab4e6f3184dbc4aabba237025fa15a03
AndréP <apmadruga63@gmail.com>
2020-07-05 01:58:11 +0000

    ascii art added

--- BlackJackServerPrompt/src/org/academiadecodigo/gitbusters/TeamCharlie/Server/Main.java
+++ BlackJackServerPrompt/src/org/academiadecodigo/gitbusters/TeamCharlie/Server/Main.java
@@ -8,6 +8,9 @@ import org.academiadecodigo.gitbusters.TeamCharlie.BlackJack.Game;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("\n               __             \n         _..-\'\'--\'----_.      \n       ,\'\'.-\'\'| .---/ _`-._   \n     ,\' \\ \\  ;| | ,/ / `-._`-.\n   ,\' ,\',\\ \\( | |// /,-._  / /\n   ;.`. `,\\ \\`| |/ / |   )/ / \n  / /`_`.\\_\\ \\| /_.-.\'-\'\'/ /  \n / /_|_:.`. \\ |;\'`..\')  / /   \n `-._`-._`.`.;`.\\  ,\'  / /    \n     `-._`.`/    ,\'-._/ /     \n       : `-/     \\`-.._/      \n       |  :      ;._ (        \n       :  |      \\  ` \\       \n        \\         \\   |       \n         :        :   ;       \n         |           /        \n         ;         ,\'         \n        /         /           \n       /         /            \n                /             \n");
+
+
         int port = ChatServer.DEFAULT_PORT;
         CardDeck cd = new CardDeck();
         Dealer dealer = new Dealer(cd);
